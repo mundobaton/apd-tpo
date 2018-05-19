0c00ee00cbe0f194f935be7fdf782edb64dc6a64
Erika Nuñez <erikannunez@gmail.com>
2018-05-19 18:53:23 -0300

    fix metodo

--- src/main/java/edu/uade/apd/tpo/controller/SistemaFacturacion.java
+++ src/main/java/edu/uade/apd/tpo/controller/SistemaFacturacion.java
@@ -35,51 +35,46 @@ public class SistemaFacturacion {
 		return instance;
 	}
 
-	private Pedido buscarPedido(Long pedidoId) {
-		return pedidoDao.findById(pedidoId);
-	}
-
 	public void facturar(Long pedidoId) {
-		//Sucede cuando despacho nos avisa que ya tiene listo el pedido para enviar
-		//Emitimos factura y remito
+		// Sucede cuando despacho nos avisa que ya tiene listo el pedido para enviar
+		// Emitimos factura y remito
 		Random rand = new Random();
-		int number = rand.nextInt(3-1) + 1;
-		
+		int number = rand.nextInt(3 - 1) + 1;
+
 		Pedido p = this.buscarPedido(pedidoId);
 		Factura f = new Factura();
 		CostoEnvio ce = new CostoEnvio();
 		ce.setZona(p.getCliente().getDomicilio().getZona());
 		ce.setTransportista(Transportista.values()[number]);
-		
+
 		f.setFecha(new Date());
 		f.setPedido(p);
 		f.setCostoEnvio(ce);
 		f.setImpuestos(new Float(0.21));
-		
+
 		if (p.getCliente().getCondIva() == CondIva.CONS_FINAL) {
 			f.setTipo(TipoFactura.B);
-		}else {
+		} else {
 			f.setTipo(TipoFactura.A);
 		}
-		
+
 		f.guardar();
-		
-		//Genero el remito
+
+		// Genero el remito
 		Remito r = new Remito();
 		r.setFactura(f);
 		r.setFecha(new Date());
 		r.guardar();
-		
+
 	}
-	
-	
+
 	public float procesarPago(String email, float importe, MedioPago mp, float saldo, float limite) {
 		List<Factura> facturas = this.obtenerFacturasImpagasCliente(email);
 		float importeRestante = (saldo + limite) + importe;
-		
-		//Ya vienen ordenadas desde la db
-		for(Factura f : facturas) {
-			if(f.getTotal() <= importeRestante) {
+
+		// Ya vienen ordenadas desde la db
+		for (Factura f : facturas) {
+			if (f.getTotal() <= importeRestante) {
 				Transaccion t = new Transaccion();
 				t.setFecha(new Date());
 				t.setImporte(f.getTotal());
@@ -90,15 +85,16 @@ public class SistemaFacturacion {
 				importeRestante -= t.getImporte();
 			}
 		}
-		//devolvemos el importe que le sobro del pago de las facturas menos el limite que le extendimos
+		// devolvemos el importe que le sobro del pago de las facturas menos el limite
+		// que le extendimos
 		return (importeRestante - limite);
 	}
 
 	public float procesarPago(Long facturaId, float importe, MedioPago mp, float saldo, float limite) {
-		
-		Factura f = obtenerFacturaById(facturaId);
+
+		Factura f = buscarFactura(facturaId);
 		float importeRestante = saldo + limite + importe;
-		if(f.getTotal() <= importeRestante){
+		if (f.getTotal() <= importeRestante) {
 			Transaccion t = new Transaccion();
 			t.setFecha(new Date());
 			t.setImporte(f.getTotal());
@@ -111,6 +107,10 @@ public class SistemaFacturacion {
 		return importeRestante - limite;
 	}
 
+	private List<Factura> obtenerFacturasCliente(String email) {
+		return facturaDao.obtenerFacturasCliente(email);
+	}
+	
 	private List<Factura> obtenerFacturasImpagasCliente(String email) {
 
 		List<Factura> facturas = this.obtenerFacturasCliente(email);
@@ -125,13 +125,12 @@ public class SistemaFacturacion {
 
 		return facturas;
 	}
-
-	private Factura obtenerFacturaById(Long facturaId){
-		return FacturaDao.getInstance().findById(facturaId);
+	
+	private Factura buscarFactura(Long facturaId) {
+		return facturaDao.findById(facturaId);
 	}
 	
-	private List<Factura> obtenerFacturasCliente(String email) {
-		return facturaDao.obtenerFacturasCliente(email);
+	private Pedido buscarPedido(Long pedidoId) {
+		return pedidoDao.findById(pedidoId);
 	}
-
 }
