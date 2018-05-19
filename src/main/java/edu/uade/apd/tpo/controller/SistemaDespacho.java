b13f4d09b0183fddb807c5e99f391af24bb9e587
Erika Nuñez <erikannunez@gmail.com>
2018-05-19 20:42:20 -0300

    pedido dao

---
+++ src/main/java/edu/uade/apd/tpo/controller/SistemaDespacho.java
@@ -0,0 +1,39 @@
+package edu.uade.apd.tpo.controller;
+
+import java.util.List;
+
+import edu.uade.apd.tpo.dao.impl.PedidoDao;
+import edu.uade.apd.tpo.model.Pedido;
+
+public class SistemaDespacho {
+
+	private static SistemaDespacho instance;
+	private PedidoDao pedidoDao;
+
+	private SistemaDespacho() {
+		this.pedidoDao = PedidoDao.getInstance();
+	}
+
+	public static SistemaDespacho getInstance() {
+	     if (instance == null) {
+	            instance = new SistemaDespacho();
+	        }
+	        return instance;
+	}
+	
+	public void despacharPedido(Long pedidoId) {
+		
+	}
+	
+	public List<Pedido> obtenerPedidosCompletos(){
+		
+		
+	}
+	
+	public Pedido buscarPedido(Long pedidoId) {
+		return pedidoDao.findById(pedidoId);
+	}
+	
+	
+	
+}
