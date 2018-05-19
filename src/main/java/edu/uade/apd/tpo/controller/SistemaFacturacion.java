e97d661a7be3f2c3cebd405ceaa472fdae103015
Erika Nuñez <erikannunez@gmail.com>
2018-05-19 17:20:05 -0300

    metodos de procesar pago by cliente y by factura id

--- src/main/java/edu/uade/apd/tpo/controller/SistemaFacturacion.java
+++ src/main/java/edu/uade/apd/tpo/controller/SistemaFacturacion.java
@@ -5,43 +5,96 @@ import edu.uade.apd.tpo.dao.impl.PedidoDao;
 import edu.uade.apd.tpo.model.Factura;
 import edu.uade.apd.tpo.model.MedioPago;
 import edu.uade.apd.tpo.model.Pedido;
+import edu.uade.apd.tpo.model.Remito;
+import edu.uade.apd.tpo.model.Transaccion;
 
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 public class SistemaFacturacion {
 
-    private static SistemaFacturacion instance;
-    private PedidoDao pedidoDao;
-    private FacturaDao facturaDao;
+	private static SistemaFacturacion instance;
+	private PedidoDao pedidoDao;
+	private FacturaDao facturaDao;
 
-    private SistemaFacturacion() {
-        this.pedidoDao = PedidoDao.getInstance();
-        this.facturaDao = FacturaDao.getInstance();
-    }
+	private SistemaFacturacion() {
+		this.pedidoDao = PedidoDao.getInstance();
+		this.facturaDao = FacturaDao.getInstance();
+	}
 
-    public static SistemaFacturacion getInstance() {
-        if (instance == null) {
-            instance = new SistemaFacturacion();
-        }
-        return instance;
-    }
+	public static SistemaFacturacion getInstance() {
+		if (instance == null) {
+			instance = new SistemaFacturacion();
+		}
+		return instance;
+	}
 
-    private Pedido buscarPedido(Long pedidoId) {
-        return pedidoDao.findById(pedidoId);
-    }
+	private Pedido buscarPedido(Long pedidoId) {
+		return pedidoDao.findById(pedidoId);
+	}
 
-    public float procesarPago(String email, float importe, MedioPago mp, float limite) {
-        List<Factura> facturas = this.obtenerFacturasCliente(email);
+	public float procesarPago(String email, float importe, MedioPago mp, float saldo, float limite) {
+		List<Factura> facturas = this.obtenerFacturasImpagasCliente(email);
+		float importeRestante = (saldo + limite) + importe;
+		
+		//Ya vienen ordenadas desde la db
+		for(Factura f : facturas) {
+			if(f.getTotal() <= importeRestante) {
+				Transaccion t = new Transaccion();
+				t.setFecha(new Date());
+				t.setImporte(f.getTotal());
+				t.setMedioPago(mp);
+				t.addFactura(f);
+				f.setTransaccion(t);
+				f.guardar();
+				importeRestante -= t.getImporte();
+			}
+		}
+		
+		//devolvemos el importe que le sobro del pago de las facturas menos el limite que le extendimos
+		return (importeRestante - limite);
+	}
 
+	public float procesarPago(Long facturaId, float importe, MedioPago mp, float saldo, float limite) {
+		
+		Factura f = obtenerFacturaById(facturaId);
+		float importeRestante = saldo + limite + importe;
+		if(f.getTotal() <= importeRestante){
+			Transaccion t = new Transaccion();
+			t.setFecha(new Date());
+			t.setImporte(f.getTotal());
+			t.setMedioPago(mp);
+			t.addFactura(f);
+			f.setTransaccion(t);
+			f.guardar();
+			importeRestante -= t.getImporte();
+		}
+		
+		return importeRestante - limite;
+	}
 
+	private List<Factura> obtenerFacturasImpagasCliente(String email) {
 
+		List<Factura> facturas = this.obtenerFacturasCliente(email);
+		Iterator<Factura> iterator = facturas.iterator();
 
-        return 0;
-    }
+		while (iterator.hasNext()) {
+			Factura f = iterator.next();
+			if (f.getTransaccion() != null) {
+				iterator.remove();
+			}
+		}
 
-    private List<Factura> obtenerFacturasCliente(String email) {
-        //TODO Filtrar el resultado y devolver facturas impagas
-        return facturaDao.obtenerFacturasCliente(email);
-    }
+		return facturas;
+	}
+
+	private Factura obtenerFacturaById(Long facturaId){
+		return FacturaDao.getInstance().findById(facturaId);
+	}
+	
+	private List<Factura> obtenerFacturasCliente(String email) {
+		return facturaDao.obtenerFacturasCliente(email);
+	}
 
 }
--- src/main/java/edu/uade/apd/tpo/model/Factura.java
+++ src/main/java/edu/uade/apd/tpo/model/Factura.java
@@ -2,78 +2,84 @@ package edu.uade.apd.tpo.model;
 
 import java.util.Date;
 
+import edu.uade.apd.tpo.dao.impl.FacturaDao;
+
 public class Factura {
 
-    private Long id;
-    private Date fecha;
-    private TipoFactura tipo;
-    private Pedido pedido;
-    private Float total;
-    private CostoEnvio costoEnvio;
-    private Float impuestos;
-    private Transaccion transaccion;
+	private Long id;
+	private Date fecha;
+	private TipoFactura tipo;
+	private Pedido pedido;
+	private Float total;
+	private CostoEnvio costoEnvio;
+	private Float impuestos;
+	private Transaccion transaccion;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Date getFecha() {
-        return fecha;
-    }
+	public Date getFecha() {
+		return fecha;
+	}
 
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
 
-    public TipoFactura getTipo() {
-        return tipo;
-    }
+	public TipoFactura getTipo() {
+		return tipo;
+	}
 
-    public void setTipo(TipoFactura tipo) {
-        this.tipo = tipo;
-    }
+	public void setTipo(TipoFactura tipo) {
+		this.tipo = tipo;
+	}
 
-    public Pedido getPedido() {
-        return pedido;
-    }
+	public Pedido getPedido() {
+		return pedido;
+	}
 
-    public void setPedido(Pedido pedido) {
-        this.pedido = pedido;
-    }
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
 
-    public Float getTotal() {
-        return total;
-    }
+	public Float getTotal() {
+		return total;
+	}
 
-    public void setTotal(Float total) {
-        this.total = total;
-    }
+	public void setTotal(Float total) {
+		this.total = total;
+	}
 
-    public CostoEnvio getCostoEnvio() {
-        return costoEnvio;
-    }
+	public CostoEnvio getCostoEnvio() {
+		return costoEnvio;
+	}
 
-    public void setCostoEnvio(CostoEnvio costoEnvio) {
-        this.costoEnvio = costoEnvio;
-    }
+	public void setCostoEnvio(CostoEnvio costoEnvio) {
+		this.costoEnvio = costoEnvio;
+	}
 
-    public Float getImpuestos() {
-        return impuestos;
-    }
+	public Float getImpuestos() {
+		return impuestos;
+	}
 
-    public void setImpuestos(Float impuestos) {
-        this.impuestos = impuestos;
-    }
+	public void setImpuestos(Float impuestos) {
+		this.impuestos = impuestos;
+	}
 
-    public Transaccion getTransaccion() {
-        return transaccion;
-    }
+	public Transaccion getTransaccion() {
+		return transaccion;
+	}
 
-    public void setTransaccion(Transaccion transaccion) {
-        this.transaccion = transaccion;
-    }
+	public void setTransaccion(Transaccion transaccion) {
+		this.transaccion = transaccion;
+	}
+
+	public void guardar() {
+		FacturaDao.getInstance().save(this);
+	}
 }
