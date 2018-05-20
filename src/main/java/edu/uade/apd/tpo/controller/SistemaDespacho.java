7955c96307d73895b17d1403fc8c4b9d3ef3f7a8
Erika Nuñez <erikannunez@gmail.com>
2018-05-20 18:29:31 -0300

    despacho del pedido

--- src/main/java/edu/uade/apd/tpo/controller/SistemaDespacho.java
+++ src/main/java/edu/uade/apd/tpo/controller/SistemaDespacho.java
@@ -3,8 +3,7 @@ package edu.uade.apd.tpo.controller;
 import java.util.List;
 
 import edu.uade.apd.tpo.dao.impl.PedidoDao;
-import edu.uade.apd.tpo.model.ItemPedido;
-import edu.uade.apd.tpo.model.Pedido;
+import edu.uade.apd.tpo.model.*;
 
 public class SistemaDespacho {
 
@@ -28,11 +27,15 @@ public class SistemaDespacho {
 
 		for(ItemPedido item : p.getItems()){
 
+			item.getArticulo().getStock().agregarMovimientoEgreso(MotivoEgreso.VENTA, item.getCantidad());
 
-
+			for(ItemLote lote : item.getLotes()){
+				for(Posicion posicion : lote.getLote().getPosiciones()){
+					SistemaDeposito.getInstance().liberarPosicion(posicion.getCodigoUbicacion(), item.getCantidad());
+				}
+			}
+			item.getArticulo().guardar();
 		}
-
-
 		SistemaFacturacion.getInstance().facturar(pedidoId);
 	}
 	
