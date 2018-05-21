492d2253c335b54e578e417fb858d601289b4ff5
Erika Nuñez <erikannunez@gmail.com>
2018-05-20 23:31:03 -0300

    fix method names

--- src/test/java/edu/uade/apd/tpo/controller/SistemaFacturacionTest.java
+++ src/test/java/edu/uade/apd/tpo/controller/SistemaFacturacionTest.java
@@ -15,15 +15,28 @@ public class SistemaFacturacionTest {
     private static final Logger logger = LoggerFactory.getLogger(SistemaAdministracion.class);
 
     @Test
-    public void testprocesarPagoImporte() throws BusinessException {
+    public void testProcesarPagoFactura() throws BusinessException {
 
-        Cliente c = ClienteDao.getInstance().findByEmail("baton2@gmail.com");
+        Cliente c = ClienteDao.getInstance().findByEmail("erikannunez@gmail.com");
 
         Long facturaId = new Long(10);
         Float importe = new Float(2000);
 
         sistemaFacturacion.procesarPago(facturaId, importe, MedioPago.EFECTIVO, c.getCuentaCorriente().getSaldo(), c.getCuentaCorriente().getLimiteCredito());
 
+    }
+
+    @Test
+    public void testProcesarPagoImporte(){
+
+        Cliente c = ClienteDao.getInstance().findByEmail("erikannunez@gmail.com");
+        sistemaFacturacion.procesarPago(c.getEmail(), new Float(2000), MedioPago.EFECTIVO,  c.getCuentaCorriente().getSaldo(), c.getCuentaCorriente().getLimiteCredito());
+
+    }
+
+    @Test
+    public void testFacturar(){
+        sistemaFacturacion.facturar(new Long(1));
     }
 
 }
