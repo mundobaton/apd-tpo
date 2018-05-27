a02124bebde8e191b69c6e02c25d0c9ff410c3eb
Erika Nuñez <erikannunez@gmail.com>
2018-05-27 03:17:08 -0300

    remito from y toEntity

--- src/main/java/edu/uade/apd/tpo/model/Remito.java
+++ src/main/java/edu/uade/apd/tpo/model/Remito.java
@@ -1,5 +1,7 @@
 package edu.uade.apd.tpo.model;
 
+import edu.uade.apd.tpo.entity.RemitoEntity;
+
 import java.util.Date;
 
 public class Remito {
@@ -27,4 +29,20 @@ public class Remito {
 
 	}
 
+	public static Remito fromEntity(RemitoEntity entity) {
+		Remito r = null;
+		if(entity != null){
+			r = new Remito();
+			r.setId(entity.getId());
+			r.setFecha(entity.getFecha());
+		}
+		return r;
+	}
+
+	public RemitoEntity toEntity(){
+		RemitoEntity entity = new RemitoEntity();
+		entity.setId(id);
+		entity.setFecha(fecha);
+		return entity;
+	}
 }
