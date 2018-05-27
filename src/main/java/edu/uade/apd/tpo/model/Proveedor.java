cf0bf87a2261cb8075e0c37b8447bb279e97639f
Erika Nuñez <erikannunez@gmail.com>
2018-05-27 03:20:19 -0300

    proveedor to y from entity

--- src/main/java/edu/uade/apd/tpo/model/Proveedor.java
+++ src/main/java/edu/uade/apd/tpo/model/Proveedor.java
@@ -1,5 +1,7 @@
 package edu.uade.apd.tpo.model;
 
+import edu.uade.apd.tpo.entity.ProveedorEntity;
+
 public class Proveedor {
 	private Long id;
 	private String nombre;
@@ -41,5 +43,24 @@ public class Proveedor {
 	public void guardar() {
 
 	}
+	public static Proveedor fromEntity(ProveedorEntity entity) {
+		Proveedor p = null;
+		if(entity != null){
+			p = new Proveedor();
+			p.setId(entity.getId());
+			p.setNombre(entity.getNombre());
+			p.setCuit(entity.getCuit());
+			p.setTelefono(entity.getTelefono());
+		}
+		return p;
+	}
 
+	public ProveedorEntity toEntity(){
+		ProveedorEntity entity = new ProveedorEntity();
+		entity.setId(id);
+		entity.setTelefono(telefono);
+		entity.setNombre(nombre);
+		entity.setCuit(cuit);
+		return entity;
+	}
 }
